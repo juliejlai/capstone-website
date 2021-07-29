0150f6bb9be2db0645f5cb2c6a6029a99e4b7414
Julie Lai <julie.lai@gladstone.ucsf.edu>
2021-07-29 15:31:12 -0700

    Making changes for authentication.

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -2,7 +2,7 @@
   <header>
     <nav>
       <ul>
-        <li><router-link to="/">Home</router-link></li>
+        <li><router-link to="/visualizer">Visualizer</router-link></li>
         <li><router-link to="/about">About</router-link></li>
         <li><router-link to="/recommender">Get Recommendations</router-link></li>
       </ul>
